780c0f16e39b38d310b7be1ba7d731177bb60121
Harry Feng <fengharryt@gmail.com>
2022-04-09 12:02:28 -0400

    framework

---
+++ FirstAndroidApp/app/src/main/java/first/android/app/WelcomeActivity.kt
@@ -0,0 +1,41 @@
+package first.android.app
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.ui.Modifier
+import first.android.app.ui.theme.FirstAndroidAppTheme
+
+class WelcomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstAndroidAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TextButton (
+                        onClick = {
+                            //val intent = Intent(Intent.ACTION_VIEW)
+                            val intent = Intent(this@WelcomeActivity, MainActivity::class.java);
+                            startActivity(intent)
+                            this.finish()
+                        },
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text("Welcome")
+                    }
+                }
+            }
+        }
+    }
+}
