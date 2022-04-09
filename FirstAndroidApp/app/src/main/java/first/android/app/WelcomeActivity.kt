bbd62930bc502249938d90e05f08cb2ee24f8dcc
Harry Feng <fengharryt@gmail.com>
2022-04-09 16:17:36 -0400

    background and welcome page

--- FirstAndroidApp/app/src/main/java/first/android/app/WelcomeActivity.kt
+++ FirstAndroidApp/app/src/main/java/first/android/app/WelcomeActivity.kt
@@ -7,13 +7,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import first.android.app.ui.theme.FirstAndroidAppTheme
+import first.android.app.ui.theme.White
 
 class WelcomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,17 +30,24 @@ class WelcomeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TextButton (
-                        onClick = {
-                            //val intent = Intent(Intent.ACTION_VIEW)
-                            val intent = Intent(this@WelcomeActivity, MainActivity::class.java);
-                            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
-                            overridePendingTransition(R.anim.slide_in_right, android.R.anim.fade_out)
-                            this.finish()
-                        },
-                        modifier = Modifier.fillMaxSize()
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
                     ) {
-                        Text("Welcome")
+                        TextButton (
+                            onClick = {
+                                //val intent = Intent(Intent.ACTION_VIEW)
+                                val intent = Intent(this@WelcomeActivity, MainActivity::class.java);
+                                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this@WelcomeActivity).toBundle())
+                                overridePendingTransition(R.anim.slide_in_right, android.R.anim.fade_out)
+                                this@WelcomeActivity.finish()
+                            },
+                            modifier = Modifier.fillMaxWidth().fillMaxHeight(.2f)
+                        ) {
+                            Text("Harry Feng's First Android App", fontWeight = FontWeight.Bold, fontSize = 20.sp, color = White)
+                        }
+                        Text("Tap anywhere to proceed...", fontSize = 16.sp, color = White)
                     }
                 }
             }
